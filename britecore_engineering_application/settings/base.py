29e61611ffe12551a5f083630978bc2cfacbc8cf
Kevin Macharia <kelvinmacharia078@gmail.com>
2019-01-21 11:09:03 +0300

    Using production settings

--- britecore_engineering_application/settings/base.py
+++ britecore_engineering_application/settings/base.py
@@ -159,6 +159,12 @@ STATIC_URL = '/static/'
 # 		}
 # 	},
 # }
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+	}
+}
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
